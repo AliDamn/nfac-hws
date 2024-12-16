ba2cdf6a3bfc3c097b06bddd930c5fb28aabf7e5
Али Биданов <alibidanov@MacBook-Air-Ali.local>
2024-12-16 20:43:24 +0500

    feat: third task completed

--- nfac-hws/homework.py
+++ nfac-hws/homework.py
@@ -7,3 +7,13 @@ print(add(3,2))
 def reverse(a):
     return a[::-1]
 print(reverse("12344"))
+
+#3
+def LengthOfString(a):
+    total = 0
+    for i in range(len(a)):
+        total += 1
+    return total
+print(LengthOfString('rainbow'))
+
+
